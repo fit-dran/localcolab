4d79de14ce961807ea3466fac60720bc4a54d2a6
Adolfo <adolfo.luna@virginiogomez.cl>
2024-06-16 15:31:42 -0400

    fix funcion de categorias

--- js/dashboard.js
+++ js/dashboard.js
@@ -34,8 +34,8 @@ function displayPosts(posts) {
             <div class="card-body">
                 <h5 class="card-title">${post.title}</h5>
                 <p class="card-text">${post.content}</p>
-                <p class="card-text"><small class="text-muted">Category: ${post.category_id}</small></p>
-                <p class="card-text"><small class="text-muted">Posted by: ${post.email}</small></p>
+                <p class="card-text"><small class="text-muted">Categoria: ${post.category_id}</small></p>
+                <p class="card-text"><small class="text-muted">Creado por: ${post.email}</small></p>
                 <div id="comments-${post.post_id}" class="mt-3"></div>
             </div>
         `;
@@ -60,7 +60,7 @@ function displayComments(postId, comments) {
         commentDiv.innerHTML = `
             <div class="card-body">
                 <p class="card-text">${comment.content}</p>
-                <p class="card-text"><small class="text-muted">Posted by: ${comment.email}</small></p>
+                <p class="card-text"><small class="text-muted">Creado por: ${comment.email}</small></p>
             </div>
         `;
         commentsDiv.appendChild(commentDiv);
